4ff132f2ee09bf7136c9fce3fc0c920c81ddbc6e
Aleksandr Myaots <amyaots@gmail.com>
2018-12-24 23:27:54 +0300

    Update main.swift

--- Sources/NexBot/main.swift
+++ Sources/NexBot/main.swift
@@ -10,20 +10,3 @@ import WebSocket
 
 let app = Application()
 app.run()
-//let worker = MultiThreadedEventLoopGroup(numberOfThreads: 2)
-//
-//let client = try HTTPClient.connect(scheme: .https, hostname: "tmi.twitch.tv", on: worker).wait()
-//print(client) // HTTPClient
-//// Create an HTTP request: GET /
-//let httpReq = HTTPRequest(method: .GET, url: "")
-//// Send the HTTP request, fetching a response
-//let httpRes = try client.send(httpReq).wait()
-//
-//let decoder = JSONDecoder()
-//decoder.keyDecodingStrategy = .convertFromSnakeCase
-//if let data = httpRes.body.data {
-//    let online = try? decoder.decode(OnlineUsers.self, from: data)
-////    try client.onClose.wait()
-//    print(online)
-//}
-
